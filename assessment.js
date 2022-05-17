92b61e6fc1512c33997284305d5126254d49d8d0
isscwong <85370331+isscwong@users.noreply.github.com>
2022-05-17 06:46:18 -0700

    Add files via upload

---
+++ assessment.js
@@ -0,0 +1,41 @@
+/*
+Indepedent Studio Services
+https://issprops.com
+
+Software Developer Candidate Assessment
+*/
+
+// #1
+// Q: What is the difference between let and var
+// A: 
+
+
+
+// #2
+// Q: Write a function that takes 2 arguments, first one is an array, second one is a string. loop through the array and find a matching result of the second argument. return the matched index of the array, if there is any
+
+
+
+// #3
+// Q: Fix the bug
+function average(a, b) {
+	return a + b / 2;
+}
+
+console.log(average(2, 1));
+
+
+// #4
+// Q: What is the output?
+let a = 22;
+let b = 'abc';
+let c = 51;
+let d = a + b + c;
+
+console.log(d);
+
+
+// #5
+// Q: Call the function in Q #2 with the following data and what is the output?
+let animals = ['Dog', 'Cat', 'Lion', 'Tiger', 'Elephant', 'Turtle', 'Bear'];
+let find = 'Whale';
