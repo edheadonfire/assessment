6fb245961d477e9404dbf6f84bd3a215761e8e7c
edheadonfire <eduardo.preciado.394@my.csun.edu>
2022-05-17 07:56:40 -0700

    Update assessment.js
    Hey I know you unsent your message but like idk I still got the email. Good luck with the employee search

--- assessment.js
+++ assessment.js
@@ -7,19 +7,29 @@ Software Developer Candidate Assessment
 
 // #1
 // Q: What is the difference between let and var
-// A: 
+// A: let is within the same scope but var is more for everywhere
 
 
 
 // #2
-// Q: Write a function that takes 2 arguments, first one is an array, second one is a string. loop through the array and find a matching result of the second argument. return the matched index of the array, if there is any
-
+// Q: Write a function that takes 2 arguments, first one is an array, second one is a string. 
+// loop through the array and find a matching result of the second argument. 
+// return the matched index of the array, if there is any
+function getIndex(array,value){
+	// I could also make sure that array is an array and that value is a value but I'm not sure that's the point of the question
+    for (let i = 0; i < array.length; i++) {
+ 		if(array[i] === value){
+            return i
+        }
+	}
+    return -1 
+}
 
 
 // #3
 // Q: Fix the bug
 function average(a, b) {
-	return a + b / 2;
+	return (a + b) / 2.0;
 }
 
 console.log(average(2, 1));
@@ -31,7 +41,7 @@ let a = 22;
 let b = 'abc';
 let c = 51;
 let d = a + b + c;
-
+// 22abc51 adding a string converts to concatination 
 console.log(d);
 
 
@@ -39,3 +49,5 @@ console.log(d);
 // Q: Call the function in Q #2 with the following data and what is the output?
 let animals = ['Dog', 'Cat', 'Lion', 'Tiger', 'Elephant', 'Turtle', 'Bear'];
 let find = 'Whale';
+// -1 other options include returning null, throwing error, false  
+// 
